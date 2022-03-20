6e306e5d10409ff2ea6382cb24b0107e8da95b88
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2022-03-20 10:59:37 +0100

    update .net to 6.0 adding several other classes

---
+++ PrimeService.Tests/PrimeService_SixthTestClass.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Xunit.Assert;
+
+    public class PrimeService_SixthTestClass
+    {
+        [Fact]
+        [TestCategory("Second category")]
+        [TestCategory("First category")]
+        [TestCategory("Simple")]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Decimal.Add(2, 2));
+        }
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Decimal.Add(2, 2));
+        }
+        
+    }
---
+++ PrimeService.Tests/PrimeService_ThirdTestClass.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_ThirdTestClass
+    {
+        [Fact]
+        [TestCategory("Second category")]
+        [TestCategory("First category")]
+        [TestCategory("Simple")]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Decimal.Add(2, 2));
+            
+        }
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Decimal.Add(2, 2));
+        }
+        
+    }
+}
